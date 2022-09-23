02a77433374c9c7a522ffb6167ebacfad47f06b5
Jenaro Murillo <jenaromurilloaldecoba@gmail.com>
2022-09-22 19:03:23 -0600

    Crear los archivos

--- Compi/src/compi/Main.java
+++ Compi/src/compi/Main.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Reader;
@@ -18,7 +19,7 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        String ruta = "D:/NetBeansProjects/Compi/src/compi/lexer.flex";
+        String ruta = "C:\\Users\\jenar\\OneDrive\\Documentos\\Compi\\scanner\\ProyectoCompi\\Compi\\src\\compi\\lexer.flex";
         generarLexer(ruta);
         probarLexer();
     }
@@ -37,24 +38,50 @@ public class Main {
 //        } catch (FileNotFoundException ex) {
 //            System.out.println("a");
 //        }
-        
+        String error = "";
+        String token = "";
+        String rutaErrores = "C:\\Users\\jenar\\OneDrive\\Documentos\\Compi\\scanner\\ProyectoCompi\\Compi\\src\\compi\\Errores.txt";
+            String rutaTokens = "C:\\Users\\jenar\\OneDrive\\Documentos\\Compi\\scanner\\ProyectoCompi\\Compi\\src\\compi\\Tokens.txt";
+        try{
+            File errorFile = new File(rutaErrores);
+            File tokenFile = new File(rutaTokens);
+            if (errorFile.createNewFile() && tokenFile.createNewFile()){
+                System.out.println("Archivos creados: " + errorFile.getName() + " " + tokenFile.getName());
+            }
+            else{
+                System.out.println("Los archivos ya existen.");
+                
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error al crear los archivos");
+            e.printStackTrace();
+        }
         try {
-            Reader lector = new BufferedReader(new FileReader("D:\\NetBeansProjects\\Compi\\src\\compi\\archivo.cpp"));
+            Reader lector = new BufferedReader(new FileReader("C:\\Users\\jenar\\OneDrive\\Documentos\\Compi\\scanner\\ProyectoCompi\\Compi\\src\\compi\\archivo.cpp"));
             Lexer lexer = new Lexer(lector);
+            FileWriter escritorErrores = new FileWriter(rutaErrores,false);
+            FileWriter escritorTokens = new FileWriter(rutaTokens,false);
             String resultado = "";
             while (true) {
                 Tokens tokens = lexer.yylex();
                 if (tokens == null) {
+                    escritorErrores.close();
+                    escritorTokens.close();
                     resultado += "FIN";
                     System.out.println(resultado);
                     return;
                 }
                 switch (tokens) {
                     case ERROR:
-                        resultado += "Simbolo no definido: "+ lexer.lexeme +" en la linea "+lexer.GetLine()+"\n";
+                        error = "Simbolo no definido: "+ lexer.lexeme +" en la linea "+lexer.GetLine()+"\n";
+                        resultado += error;
+                        escritorErrores.write(error);
                         break;
                     default:
-                        resultado += lexer.lexeme + ": Es un " + tokens + " en la linea "+lexer.GetLine()+"\n";
+                        token = lexer.lexeme + ": Es un " + tokens + " en la linea "+lexer.GetLine()+"\n";
+                        resultado += token ;
+                        escritorTokens.write(token);
                         break;
                 }
             }
