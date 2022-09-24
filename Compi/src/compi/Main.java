f0ec8d0031f56b7b51237bad19e8e29d97aa718b
Jenaro Murillo <jenaromurilloaldecoba@gmail.com>
2022-09-23 21:38:00 -0600

    Arreglos

--- Compi/src/compi/Main.java
+++ Compi/src/compi/Main.java
@@ -11,6 +11,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -38,50 +40,48 @@ public class Main {
 //        } catch (FileNotFoundException ex) {
 //            System.out.println("a");
 //        }
-        String error = "";
-        String token = "";
-        String rutaErrores = "C:\\Users\\jenar\\OneDrive\\Documentos\\Compi\\scanner\\ProyectoCompi\\Compi\\src\\compi\\Errores.txt";
-            String rutaTokens = "C:\\Users\\jenar\\OneDrive\\Documentos\\Compi\\scanner\\ProyectoCompi\\Compi\\src\\compi\\Tokens.txt";
-        try{
-            File errorFile = new File(rutaErrores);
-            File tokenFile = new File(rutaTokens);
-            if (errorFile.createNewFile() && tokenFile.createNewFile()){
-                System.out.println("Archivos creados: " + errorFile.getName() + " " + tokenFile.getName());
-            }
-            else{
-                System.out.println("Los archivos ya existen.");
-                
-            }
-        }
-        catch (IOException e){
-            System.out.println("Error al crear los archivos");
-            e.printStackTrace();
-        }
         try {
             Reader lector = new BufferedReader(new FileReader("C:\\Users\\jenar\\OneDrive\\Documentos\\Compi\\scanner\\ProyectoCompi\\Compi\\src\\compi\\archivo.cpp"));
             Lexer lexer = new Lexer(lector);
-            FileWriter escritorErrores = new FileWriter(rutaErrores,false);
-            FileWriter escritorTokens = new FileWriter(rutaTokens,false);
+            HashMap<String,ArrayList<String>> token = new HashMap<String,ArrayList<String>>();
+            HashMap<String,ArrayList<String>> error = new HashMap<String,ArrayList<String>>();
             String resultado = "";
+            int cont = 1;
             while (true) {
                 Tokens tokens = lexer.yylex();
                 if (tokens == null) {
-                    escritorErrores.close();
-                    escritorTokens.close();
                     resultado += "FIN";
                     System.out.println(resultado);
+                    System.out.println(token);
+                    System.out.println(error);
+                    crearArchivoError(error);
+                    crearArchivoToken(token);
                     return;
                 }
                 switch (tokens) {
                     case ERROR:
-                        error = "Simbolo no definido: "+ lexer.lexeme +" en la linea "+lexer.GetLine()+"\n";
-                        resultado += error;
-                        escritorErrores.write(error);
+                        resultado += "Simbolo no definido: "+ lexer.lexeme +" en la linea "+lexer.GetLine()+"\n";
+                         if (error.containsKey(lexer.lexeme)){
+                            error.get(lexer.lexeme).add(String.valueOf(lexer.GetLine()));
+                        }
+                        else{
+                            error.put(lexer.lexeme, new ArrayList<String>());
+                            error.get(lexer.lexeme).add(String.valueOf("Simbolo no definido"));
+                            error.get(lexer.lexeme).add(String.valueOf(lexer.GetLine()));
+                         }
+                        //escritorErrores.write(lexer.lexeme +"    |      "+"Simbolo no definido"+"    |      "+lexer.GetLine()+"\n");
                         break;
                     default:
-                        token = lexer.lexeme + ": Es un " + tokens + " en la linea "+lexer.GetLine()+"\n";
-                        resultado += token ;
-                        escritorTokens.write(token);
+                        resultado += lexer.lexeme + ": Es un " + tokens + " en la linea "+lexer.GetLine()+"\n";
+                        if (token.containsKey(lexer.lexeme)){
+                            token.get(lexer.lexeme).add(String.valueOf(lexer.GetLine()));
+                        }
+                        else{
+                            token.put(lexer.lexeme, new ArrayList<String>());
+                            token.get(lexer.lexeme).add(String.valueOf(tokens));
+                            token.get(lexer.lexeme).add(String.valueOf(lexer.GetLine()));
+                        }
+                        //escritorTokens.write(lexer.lexeme + "    |      " + tokens + "    |      "+lexer.GetLine()+"\n");
                         break;
                 }
             }
@@ -92,5 +92,113 @@ public class Main {
         }
 
     }
+    
+    public static void crearArchivoError(HashMap<String,ArrayList<String>> errores){
+        String rutaErrores = "C:\\Users\\jenar\\OneDrive\\Documentos\\Compi\\scanner\\ProyectoCompi\\Compi\\src\\compi\\Errores.txt";
+        try{
+            File errorFile = new File(rutaErrores);
+            if (errorFile.createNewFile()){
+                System.out.println("Archivo creado: " + errorFile.getName());
+            }
+            else{
+                System.out.println("El archivo de errores ya existe.");
+                
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error al crear los archivos");
+            e.printStackTrace();
+        }
+        try{
+            FileWriter escritorErrores = new FileWriter(rutaErrores,false);
+            escritorErrores.write("Token    |      Tipo de Error    |     Linea" + "\n");
+            for (String error:errores.keySet()){
+                ArrayList<String> valor = errores.get(error);
+                escritorErrores.write(error + "    |      ");
+                escritorErrores.write(valor.get(0) + "    |      ");
+                HashMap<String, Integer> lineas = new HashMap<>();
+
+                for(int i = 1; i<valor.size(); i++) {
+                    String numLinea = valor.get(i);
+                    if(lineas.containsKey(numLinea)) {
+                        lineas.put(numLinea, lineas.get(numLinea)+1);
+                    } else {
+                        lineas.put(numLinea, 1);
+                    }
+                    
+                }
+                for (String linea:lineas.keySet()){
+                    if (lineas.get(linea) > 1)
+                        escritorErrores.write(linea+"(" + lineas.get(linea) + ")" + " ");
+                    else
+                        escritorErrores.write(linea + " ");
+                }
+                escritorErrores.write("\n");
+            }
+            
+            
+            
+            escritorErrores.close();
+            
+        }
+        catch (IOException e){
+            System.out.println("Error al escribir los archivos");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void crearArchivoToken(HashMap<String,ArrayList<String>> tokens){
+        String rutaTokens = "C:\\Users\\jenar\\OneDrive\\Documentos\\Compi\\scanner\\ProyectoCompi\\Compi\\src\\compi\\Tokens.txt";
+        try{
+            File tokenFile = new File(rutaTokens);
+            if (tokenFile.createNewFile()){
+                System.out.println("Archivo creado: " + tokenFile.getName());
+            }
+            else{
+                System.out.println("El archivo tokens ya existe.");
+                
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error al crear los archivos");
+            e.printStackTrace();
+        }
+        try{
+            FileWriter escritorTokens = new FileWriter(rutaTokens,false);
+            escritorTokens.write("Token    |      Tipo de Token    |      Linea" + "\n");
+            for (String token:tokens.keySet()){
+                ArrayList<String> valor = tokens.get(token);
+                escritorTokens.write(token + "    |      ");
+                escritorTokens.write(valor.get(0) + "    |      ");
+               HashMap<String, Integer> lineas = new HashMap<>();
+
+                for(int i = 1; i<valor.size(); i++) {
+                    String numLinea = valor.get(i);
+                    if(lineas.containsKey(numLinea)) {
+                        lineas.put(numLinea, lineas.get(numLinea)+1);
+                    } else {
+                        lineas.put(numLinea, 1);
+                    }
+                    
+                }
+                for (String linea:lineas.keySet()){
+                    if (lineas.get(linea) > 1)
+                        escritorTokens.write(linea +"(" + lineas.get(linea) + ")" + " ");
+                    else
+                        escritorTokens.write(linea + " ");
+                }
+               escritorTokens.write("\n");
+            }
+            
+            escritorTokens.close();
+            
+        }
+        catch (IOException e){
+            System.out.println("Error al escribir los archivos");
+            e.printStackTrace();
+        }
+    }
+    
+    
 }
 
