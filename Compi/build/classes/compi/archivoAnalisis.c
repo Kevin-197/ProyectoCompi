9e565b881813a1c23a68fcc0cc403fc3f9663fb1
Kevin Quesada <kevin.qj@estudiantec.cr>
2022-11-06 14:18:17 -0600

    Recuperacion de errores casi completa

--- Compi/build/classes/compi/archivoAnalisis.c
+++ Compi/build/classes/compi/archivoAnalisis.c
@@ -1,13 +1,34 @@
-#define prueba 3
-int main()
-{
-    5+5;
-    prueba(1 , 2,3);
-    if(1===1){
-        ap+;
+//#define prueba 
+//int prueba = ;
+int main() {
+    5+;
+    switch(prueba){
+        case 5:
+            a+;
+        break;
+        case 5:
+            a+4;
+        break;
+        case p:
+            a+;
+        break;
+        case 5:
+            a+;
+        break;
+        case p:
+            a+5;
+        break;
     }
- int c = ;
- int c = ;
+    
+    int c = ;
+    a+4;
+    c  prueba(1 , 2,3);
+    while(a==1){
+        ap+5;
+    }
+    int c = 5;
+    int c = 5;
+    a + +;
     a + +5;
-    //; 5*45+int;
+    4; 5*45+;
 }
