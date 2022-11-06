dde0df449029df45f864d1d29556bee7d31cf2fe
Kevin Quesada <kevin.qj@estudiantec.cr>
2022-11-06 16:46:33 -0600

    Recuperacion de errores lista
    creo...

--- Compi/build/classes/compi/archivoAnalisis.c
+++ Compi/build/classes/compi/archivoAnalisis.c
@@ -1,3 +1,66 @@
+#define p ;
+int prueba = ;
+int prueba = ;
+void main(1){
+    int input, stop;
+    stop = 1;
+    while ((input = getchar()) != EOF) {
+        stop 1 =;
+        if (input == '\t' ) {
+            putchar('\\');
+            putchar('t');
+            stop = ;
+        }
+        if (input == ' ') {
+            putchar('\\');
+            putchar('b')
+            stop = 0;
+        }
+        if (input == '\\') {
+            putchar('\\');
+            putchar('\\';
+            stop = 0;
+        }
+        else {
+            if (stop == 1) {
+                printf('%c', input);
+            }
+        }
+ 
+    }
+}
+#define a ;
+int prueba = 1;
+void main(){
+    int input, stop;
+    stop = 1;
+    while ((input = getchar()) != EOF) {
+        stop 1 =;
+        if (input == '\t' ) {
+            putchar('\\');
+            putchar('t');
+            stop = ;
+        }
+        if (input == ' ') {
+            putchar('\\');
+            putchar('b')
+            stop = 0;
+        }
+        if (input == '\\') {
+            putchar('\\');
+            putchar('\\';
+            stop = 0;
+        }
+        else {
+            if (stop == 1) {
+                printf('%c', input);
+            }
+        }
+ 
+    }
+}
+#define a p
+int prueba = ;
 void main(){
     int input, stop;
     stop = 1;
